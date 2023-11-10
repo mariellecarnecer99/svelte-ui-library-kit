d182fac4494f155ae97c03cb2b9f2fd94ba2dfc2
maynion <mcarnecer@startechup.com>
2023-11-10 12:12:09 +0800

    forminputs and icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,8 @@
 	import Button from '$lib/components/button/Button.svelte';
 	import Grid from '$lib/components/grid/Grid.svelte';
 	import GridItem from '$lib/components/grid/GridItem.svelte';
+	import FormInput from '$lib/components/form/FormInput.svelte';
+	import Icon from "$lib/components/icon/Icon.svelte";
 </script>
 
 <section>
@@ -22,6 +24,13 @@
 		<GridItem col="6">6</GridItem>
 		<GridItem col="6">6</GridItem>
 	</Grid>
+
+	<FormInput col="6" for="fname" label="First Name" type="text" name="firstname"></FormInput>
+	<FormInput col="6" for="lname" label="Last Name" type="text" name="lastname"></FormInput>
+	<FormInput col="12" for="username" label="Username" type="text" name="username"></FormInput>
+
+	<Icon type="brands" name="wikipedia-w" />
+	<Icon type="solid" name="house" />
 </section>
 
 <style>
