8fc166fdcf5c0aa27e862e0f4135cc42ff3c525d
maynion <mcarnecer@startechup.com>
2023-11-17 19:10:17 +0800

    align and justify

--- src/lib/row-col/Row.svelte
+++ src/lib/row-col/Row.svelte
@@ -1,5 +1,12 @@
+<script>
+	let justify;
+	let align;
+
+	export { justify, align };
+</script>
+
 <div class="container">
-	<div class="row">
+	<div class="row justify-{justify} align-{align}">
 		<slot />
 	</div>
 </div>
@@ -14,5 +21,37 @@
 		display: flex;
 		/* flex-wrap: wrap; */
 		margin-top: 25px;
+	}
+
+	.align-top {
+		vertical-align: top;
+	}
+
+	.align-middle {
+		vertical-align: middle;
+	}
+
+	.align-bottom {
+		vertical-align: bottom;
+	}
+
+	.justify-start {
+		justify-content: start;
+	}
+
+	.justify-end {
+		justify-content: end;
+	}
+
+	.justify-center {
+		justify-content: center;
+	}
+
+	.justify-space-around {
+		justify-content: space-around;
+	}
+
+	.justify-space-between {
+		justify-content: space-between;
 	}
 </style>
