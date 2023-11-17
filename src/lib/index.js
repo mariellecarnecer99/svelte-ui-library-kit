3e372fefbe9aa88ec3665e792a7d7aed6e5ef938
maynion <mcarnecer@startechup.com>
2023-11-17 11:51:01 +0800

    fixes

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,8 +1,8 @@
 // Reexport your entry components here
-export { default as Button } from './components/button/Button.svelte';
-export { default as Grid } from './components/grid/Grid.svelte';
-export { default as GridItem } from './components/grid/GridItem.svelte';
-export { default as FormInput } from './components/form/FormInput.svelte';
-export { default as Icon } from './components/icon/Icon.svelte';
-export { default as Sidebar } from './components/sidebar/Sidebar.svelte';
-export { default as SidebarItem } from './components/sidebar/SidebarItem.svelte';
+export { default as Button } from './button/Button.svelte';
+export { default as Grid } from './grid/Grid.svelte';
+export { default as GridItem } from './grid/GridItem.svelte';
+export { default as FormInput } from './form/FormInput.svelte';
+export { default as Icon } from './icon/Icon.svelte';
+export { default as Sidebar } from './sidebar/Sidebar.svelte';
+export { default as SidebarItem } from './sidebar/SidebarItem.svelte';
